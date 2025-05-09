3e1586d189a2eb47b079c042c66ab766a8244f79
Malcom <micsha2604@gmail.com>
2025-05-09 22:04:11 +0530

    Update Reg.v

--- Reg.v
+++ Reg.v
@@ -10,26 +10,10 @@ output reg [31:0]Regdata1,
 output reg [31:0]Regdata2,
 output [3:0]riscout
 
-//,output [31:0]regbank0,
-//output [31:0]regbank1,
-//output [31:0]regbank2,
-//output [31:0]regbank3,
-//output [31:0]regbank4,
-//output [31:0]regbank5,
-//output [31:0]regbank6,
-//output [31:0]regbank7
 );
 
 reg [31:0]RegisterBank[0:31];
-
-//assign regbank0 = RegisterBank[0];
-//assign regbank1 = RegisterBank[1];
-//assign regbank2 = RegisterBank[2];
-//assign regbank3 = RegisterBank[3];
-//assign regbank4 = RegisterBank[4];
-//assign regbank5 = RegisterBank[5];
-//assign regbank6 = RegisterBank[6];
-//assign regbank7 = RegisterBank[7];
+    
 assign riscout = writedata[3:0];
 
 
@@ -52,23 +36,8 @@ always@(posedge clk or negedge reset )begin
         if(RegWrite == 1)
             RegisterBank[writeReg] <= writedata;
     end      
-//riscout <= {RegisterBank[1][3:0],RegisterBank[2][3:0],
-//                RegisterBank[3][3:0],RegisterBank[4][3:0]};   
+
 end
 
-//always@(posedge reset)begin
-//        for(i=0;i<32;i=i+1)begin
-//            RegisterBank[i] <= 0;
-//            end
-//        end
-
-//always@(posedge clk )begin
-
-//        if(RegWrite == 1)
-//            RegisterBank[writeReg] <= writedata;
-      
-//riscout <= {RegisterBank[1][3:0],RegisterBank[2][3:0],
-//                RegisterBank[3][3:0],RegisterBank[4][3:0]};   
-//end
 
 endmodule
